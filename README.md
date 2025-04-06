d7d087a13b805ad1f448a30d53c122030882b73e
huseinzol05 <husein.zol05@gmail.com>
2025-04-07 00:20:01 +0800

    initial tp lol

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # huseinzol05.github.io
-Sloppy page for huseinzol05
+
+Sloppy page for huseinzol05, it is a very minimal setup, basically I convert from Jupyter Notebook to simple HTML page,
+
+```bash
+python3 run.py
+```
---
+++ run.py
@@ -0,0 +1,71 @@
+from glob import glob
+from nbconvert import HTMLExporter
+import nbformat
+import os
+
+template = """
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="{{description}}" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@huseinzol05" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="Husein Zolkepli" />
+
+    <style>
+      body {
+        line-height: 1.4;
+        font-size: 16px;
+        padding: 0 10px;
+        margin: 50px auto;
+        max-width: 1000px;
+      }
+
+      #maincontent {
+        max-width: 62em;
+        margin: 15 auto;
+      }
+    </style>
+
+    <title>{{title}}</title>
+  </head>
+
+  <body>
+    <div id="maincontent" style="margin-top: 70px">
+    {{body}}
+    </div>
+  </body>
+</html>
+"""
+
+index = """
+<h2>Husein Zolkepli</h2>
+
+<p>
+I'm a software engineer with experience in distributed Large Model training, efficient LLM serving, big data systems and processing, and infrastructure engineering.
+I open source Malaysian multimodality models at <a href="https://github.com/mesolitica">https://github.com/mesolitica</a>
+and <a href="https://huggingface.co/mesolitica">https://huggingface.co/mesolitica</a>
+</p>
+"""
+
+def replace(description, title, body):
+    t = template.replace('{{description}}', description)
+    t = t.replace('{{title}}', title)
+    return t.replace('{{body}}', body)
+
+with open('index.html', 'w') as fopen:
+    
+    index = replace(description = 'A few things from Husein Zolkepli', title = 'Husein Zolkepli', body = index)
+    fopen.write(index)
+
+files = glob('*/*.ipynb')
+for f in files:
+  title = os.path.split(f)[1].split('.')[0]
+  notebook = nbformat.read(open(f), as_version=4)
+  html_exporter = HTMLExporter(
+      template_file = 'base', exclude_input_prompt = False, exclude_output_prompt = False)
+  (html, resources) = html_exporter.from_notebook_node(notebook)
+  html = html.replace('<img alt', '<img width="100%" alt')
+  with open(f.replace('.ipynb', '.html'), "w", encoding="utf-8") as f:
+    f.write(replace(title, title, html))
